8db35cf523c80234bd37f2825823497f5d83fc43
akinosu <g031l116@gmail.com>
2022-03-03 09:42:58 +0900

    update 入力エラー時、エラー文をトップページに表示

--- html/post.php
+++ html/post.php
@@ -17,23 +17,23 @@ if( preg_match( '/^[0-9]+$/', $tel ) == false ) {
     //入力された電話番号が数値であるか確認
     // $telが半角数字でない場合
     $error_message = "入力された電話番号をご確認ください。半角数字以外が使用されています。";
-    include "error.html";
+    include "index.html";
 }elseif( 50 < mb_strlen($name, 'UTF-8') ) {
     // 名前の文字数を確認
     $error_message = '名前は50文字以内で入力してください。';
-    include "error.html";
+    include "index.html";
 }elseif( 50 < mb_strlen($email, 'UTF-8') ) {
     // メールアドレスの文字数を確認
     $error_message = 'メールアドレスは50文字以内で入力してください。';
-    include "error.html";
+    include "index.html";
 }elseif( 100 < mb_strlen($subject, 'UTF-8') ) {
     // 件名の文字数を確認
     $error_message = '件名は100文字以内で入力してください。';
-    include "error.html";
+    include "index.html";
 }elseif( 500 < mb_strlen($message, 'UTF-8') ) {
     // 本文の文字数を確認
     $error_message = 'お問い合わせ内容は500文字以内で入力してください。';
-    include "error.html";
+    include "index.html";
 }else{
     include "conf.html";
 }
